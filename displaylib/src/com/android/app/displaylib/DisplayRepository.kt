7af52305888ae2146c2fc46e635b1a228db1bcc1
Nicolo' Mazzucato <nicomazz@google.com>
2026-02-06 15:40:05 +0000

    Fix sticky display icon after closing closing laptop lid and removing cable
    Now we're also listening at display type changes to determine the status of the "display" statusbar icon.
    
    This fixes the case where the icon was sticky when
    - device connected to external display with lid closed
    - cable disconnected
    - lid opened
    as the display list didn't change (but only the default display type did)
    
    Bug: 459360385
    Test: DisplayRepositoryTest
    Flag: NONE - small bugfix
    Change-Id: I5d11e94f2b5ba42cb24183d4d57032b919765a54

--- displaylib/src/com/android/app/displaylib/DisplayRepository.kt
+++ displaylib/src/com/android/app/displaylib/DisplayRepository.kt
@@ -86,6 +86,9 @@ interface DisplayRepository {
      */
     val pendingDisplay: Flow<PendingDisplay?>
 
+    /** The type of the default display. */
+    val defaultDisplayType: StateFlow<Int>
+
     /** Whether the default display is currently off. */
     val defaultDisplayOff: StateFlow<Boolean>
 
@@ -435,6 +438,12 @@ constructor(
                 SharingStarted.WhileSubscribed(),
                 defaultDisplay.state == Display.STATE_OFF,
             )
+
+    override val defaultDisplayType: StateFlow<Int> =
+        displayChangeEvent
+            .filter { it == Display.DEFAULT_DISPLAY }
+            .map { defaultDisplay.type }
+            .stateIn(bgApplicationScope, SharingStarted.WhileSubscribed(), defaultDisplay.type)
 
     override fun getDisplay(displayId: Int): Display? {
         val cachedDisplay = getCachedDisplay(displayId)
